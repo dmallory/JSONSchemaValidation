02e536f4ffd54040bf19e8e21295b29eac797492
Dustin Mallory <dmallory@doximity.com>
2021-01-27 14:06:22 -0700

    Set ios version to 9

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "DSJSONSchemaValidation",
-    platforms: [.iOS(.v8), .tvOS(.v9), .macOS(.v10_10)],
+    platforms: [.iOS(.v9), .tvOS(.v9), .macOS(.v10_10)],
     products: [
         .library(
             name: "DSJSONSchemaValidation",
